4977f0c8fbb3f51ce1c47189f3adee4aa78a026b
kotmotok <kristina251193@mail.ru>
2023-01-14 20:17:18 +0300

    добавила задачу 2.4 в проект

---
+++ lesson2.4.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    text = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    button = browser.find_element(By.ID, "book")
+    button.click()
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    
+    input = browser.find_element(By.ID, "answer")
+    input.send_keys(y)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.XPATH, "//button[text()='Submit']")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
